067def8bf27c73c41370a92fe75e566489505193
yerimk9 <rhrh037@naver.com>
2024-07-26 15:10:37 +0900

    세부 레이아웃 조정 중입니당 - 1

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -9,6 +9,14 @@ function MainPage() {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
 
+      // 맨 위로 스크롤이 올라가면 첫 번째 이미지를 보이게 설정
+      if (scrollPosition === 0) {
+        setCurrentImage(
+          "https://i.pinimg.com/originals/b5/9d/8f/b59d8f8cbb54368862109db8324dc6b8.jpg"
+        );
+        return;
+      }
+
       const sections = document.querySelectorAll(".content");
       sections.forEach((section) => {
         const sectionTop = section.offsetTop;
@@ -49,8 +57,8 @@ function MainPage() {
           <div className="center-txt">
             <p className="page-heading">
               <span className="page-sub">profile·about me </span>
+              <p className="">김예림</p>
             </p>
-            <p className="name-en">김예림</p>
           </div>
           <a href="/" className="read_more scroll">
             <span>r</span>
@@ -196,49 +204,52 @@ function MainPage() {
             </div>
           </div>
         </section>
+        <section className="content img-change">
+          <div className="fix-img">
+            <div className="img-box">
+              <img
+                src="https://i.pinimg.com/originals/f2/b5/5e/f2b55eb862091d8c61a48da100811c98.jpg"
+                alt="img2"
+              />
+            </div>
+          </div>
+          <div className="inner-content">
+            <div className="txt-wrap">
+              <p className="sub-heading">
+                <span className="num">00</span>
+                내일
+              </p>
+              <p className="sec-heading">김예림</p>
+              <div className="sec-txt">
+                <p className="p1">주식회사 온후 대표</p>
+                <p className="p1">디자인 파트너 “여기” 주재</p>
+                <p>&nbsp;</p>
+                <p className="p1">주식회사 코스기유 커먼 디자이너</p>
+                <p>주식회사 추출사 커먼 디자이너</p>
+                <p className="p1">
+                  주식회사 목욕탕 구라시 아트 디렉터/디자이너
+                </p>
+                <p>&nbsp;</p>
+                <p className="p1">
+                  사이타마현 사이타마시 출생. 나가노현 스와시 거주{" "}
+                  <span className="s1">2022</span>년{" "}
+                  <span className="s1">11</span> 월~). 디자인계 대학을 졸업 후,
+                  디자인 제작 회사에서 그래픽 디자이너를 거쳐{" "}
+                  <span className="s1">2020</span> 년 독립. 로고 디자인을 비롯해
+                  각종 그래픽, 웹, 사인 계획 등의 「표시」를 만들고, 「여기」가
+                  즐길 때까지를 디자인할 수 있도록 시도하고 있습니다.
+                  <span className="s1">2021</span> 년부터 주식회사 고스기유와
+                  주식회사 목욕탕 구라시, 2023년부터 주식회사 추출사에 참가{" "}
+                  <span className="s1">2023</span> 년 법인화, 주식회사 온후
+                  설립.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <section className="other-content">
-          <div className="other-box">
-            <p className="tit inline">
-              <img
-                src="https://i.pinimg.com/originals/54/5b/36/545b3655d0c16b5e65053694642b622b.jpg"
-                alt="img3"
-              />
-            </p>
-            <div className="flex-box">
-              <p className="box02 txt">다카기 야스히라 웹 디자이너</p>
-              <p className="box02 txt">이노우 마사히로 편집자</p>
-              <p className="box02 txt">카토 오마사 스쿠</p>
-              <p className="box02 txt">카와무라 에히코 웹 엔지니어</p>
-              <p className="box02 txt">나가미네 쇼타 웹 엔지니어</p>
-              <p className="box02 txt">후지와라 케이 포토그래퍼</p>
-              <p className="box02 txt">折茂彰弘 크리에이티브 디렉터</p>
-              <p className="box02 txt">엔 피츠오 레터링 아티스트</p>
-              <p className="box02 txt">히라마츠 유스케 고스기유 3대째</p>
-              <p className="box02 txt">오야마 카즈히로 차 리스타</p>
-              <p className="box02 txt">오자와 히로유키 프로듀서</p>
-            </div>
-          </div>
-          <div className="other-box">
-            <p className="tit inline">
-              <img
-                src="https://i.pinimg.com/originals/9f/89/fb/9f89fb5d73eb2b5ba7b3f77533b88a70.jpg"
-                alt="img3"
-              />
-            </p>
-            <div className="flex-box">
-              <p className="box02 txt">다카기 야스히라 웹 디자이너</p>
-              <p className="box02 txt">이노우 마사히로 편집자</p>
-              <p className="box02 txt">카토 오마사 스쿠</p>
-              <p className="box02 txt">카와무라 에히코 웹 엔지니어</p>
-              <p className="box02 txt">나가미네 쇼타 웹 엔지니어</p>
-              <p className="box02 txt">후지와라 케이 포토그래퍼</p>
-              <p className="box02 txt">折茂彰弘 크리에이티브 디렉터</p>
-              <p className="box02 txt">엔 피츠오 레터링 아티스트</p>
-              <p className="box02 txt">히라마츠 유스케 고스기유 3대째</p>
-              <p className="box02 txt">오야마 카즈히로 차 리스타</p>
-              <p className="box02 txt">오자와 히로유키 프로듀서</p>
-            </div>
-          </div>
           <div className="sns-box">
             <div className="flex-box">
               <p className="tit">SNS</p>
@@ -322,94 +333,86 @@ function MainPage() {
             </div>
           </div>
         </section>
-      </div>
 
-      <div className="sp_bg sp_only">
-        <div className="bg-wrap">
-          <div className="bg-box-inner">
-            <div className="frame">
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="fix-content fix-left">
-        <div className="box-top">
-          <h1 className="header_title">
-            <div className="titleLogo-container">
-              <div className="titleLogo-content">
-                <svg
-                  className="titleLogo-subTitle"
-                  viewBox="0 0 450 200"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    id="curve"
-                    d="M10,55 C150,20 390,10 690,65"
-                    fill="transparent"
-                  />
-                  <text>
-                    <textPath
-                      href="#curve"
-                      startOffset="50%"
-                      textAnchor="middle"
-                    >
-                      welcome
-                    </textPath>
-                  </text>
-                </svg>
-                <h1 className="titleLogo-title">김포폴</h1>
+        <div className="fix-content fix-left">
+          <div className="box-top">
+            <h1 className="header_title">
+              <div className="titleLogo-container">
+                <div className="titleLogo-content">
+                  <svg
+                    className="titleLogo-subTitle"
+                    viewBox="0 0 450 200"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      id="curve"
+                      d="M10,55 C150,20 390,10 690,65"
+                      fill="transparent"
+                    />
+                    <text>
+                      <textPath
+                        href="#curve"
+                        startOffset="50%"
+                        textAnchor="middle"
+                      >
+                        welcome
+                      </textPath>
+                    </text>
+                  </svg>
+                  <h1 className="titleLogo-title">김포폴</h1>
+                </div>
               </div>
-            </div>
-          </h1>
-        </div>
-        <div className="box-middle">
-          <div className="img-box"></div>
-        </div>
-        <div className="box-bottom">
-          <nav className="nav">
-            <ul className="nav_lists">
-              <li className="nav_list">
-                <a className="nav_link" href="https://cocochi.design">
-                  집<span>홈</span>
-                </a>
-              </li>
-              <li className="nav_list">
-                <a
-                  className="nav_link"
-                  href="https://cocochi.design/#linkThink"
-                >
-                  기모치<span>attitude</span>
-                </a>
-              </li>
-              <li className="nav_list">
-                <a className="nav_link" href="https://cocochi.design/#linkWork">
-                  시루시<span>works</span>
-                </a>
-              </li>
-              <li className="nav_list">
-                <a className="nav_link" href="https://cocochi.design/profile">
-                  나<span>프로필</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="nav_other">
-            <p className="text-link contact-btn">
-              <a className="fuwafuwa1" href="https://cocochi.design/contact">
-                <img
-                  src="https://cocochi.design/wp-content/themes/cocochi/images/btn_contact.svg"
-                  alt=""
-                />
-              </a>
-            </p>
-            <p className="credit">copyright onf Inc.</p>
+            </h1>
           </div>
-        </div>
-        <div className="nav_trigger">
-          <span></span>
+          <div className="box-middle">
+            <div className="img-box"></div>
+          </div>
+          <div className="box-bottom">
+            <nav className="nav">
+              <ul className="nav_lists">
+                <li className="nav_list">
+                  <a className="nav_link" href="https://cocochi.design">
+                    집<span>홈</span>
+                  </a>
+                </li>
+                <li className="nav_list">
+                  <a
+                    className="nav_link"
+                    href="https://cocochi.design/#linkThink"
+                  >
+                    기모치<span>attitude</span>
+                  </a>
+                </li>
+                <li className="nav_list">
+                  <a
+                    className="nav_link"
+                    href="https://cocochi.design/#linkWork"
+                  >
+                    시루시<span>works</span>
+                  </a>
+                </li>
+                <li className="nav_list">
+                  <a className="nav_link" href="https://cocochi.design/profile">
+                    나<span>프로필</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="nav_other">
+              <p className="text-link contact-btn">
+                <a className="fuwafuwa1" href="https://cocochi.design/contact">
+                  <img
+                    src="https://cocochi.design/wp-content/themes/cocochi/images/btn_contact.svg"
+                    alt=""
+                  />
+                </a>
+              </p>
+              <p className="credit">copyright onf Inc.</p>
+            </div>
+          </div>
+          <div className="nav_trigger">
+            <span></span>
+          </div>
         </div>
       </div>
     </div>
