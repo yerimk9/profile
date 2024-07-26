199a6d6fad104298430025cd152c21aada82d00a
yerimk9 <rhrh037@naver.com>
2024-07-26 11:08:36 +0900

    html 구조 구현

---
+++ src/components/Logo.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect } from "react";
+import gsap from "gsap";
+
+function Logo() {
+  useEffect(() => {
+    const splitText = (el) => {
+      el.innerHTML = el.textContent.replace(
+        /(\S)/g,
+        '<span class="char" style="display: inline-block; transform: translateY(100%); opacity: 0;">$1</span>'
+      );
+    };
+
+    const title = document.querySelector(".logo-title");
+
+    splitText(title);
+
+    const timeline = gsap.timeline();
+
+    timeline
+      .to(".logo-title .char", {
+        y: 0,
+        opacity: 1,
+        stagger: 0.05,
+        ease: "bounce.out",
+        duration: 2,
+      })
+      .to(
+        ".logo-title .char",
+        {
+          opacity: (index, target) => {
+            const char = target.textContent;
+            return ["김", "포", "폴"].includes(char) ? 1 : 0;
+          },
+          duration: 1,
+          ease: "power2.out",
+        },
+        "+=1"
+      )
+      .to(".logo-title", {
+        letterSpacing: "0rem",
+        duration: 1.5,
+        ease: "power2.out",
+      })
+      .to(
+        ".logo-title .char",
+        {
+          x: (index, target) => {
+            const char = target.textContent;
+            if (char === "김") {
+              return 50;
+            }
+            return 0;
+          },
+          duration: 1.5,
+          ease: "power2.out",
+          stagger: 0.1,
+        },
+        "-=1"
+      )
+      .to(
+        ".logo-subTitle",
+        {
+          y: "13%",
+          opacity: 1,
+          ease: "bounce.out",
+          duration: 1,
+        },
+        "-=0.5"
+      );
+  }, []);
+
+  return (
+    <div className="logo-container">
+      <div className="logo-content">
+        <svg
+          className="logo-subTitle"
+          viewBox="0 0 500 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            id="curve"
+            d="M10,65 C150,10 350,10 490,80"
+            fill="transparent"
+          />
+          <text>
+            <textPath href="#curve" startOffset="50%" textAnchor="middle">
+              welcome
+            </textPath>
+          </text>
+        </svg>
+        <h1 className="logo-title">김예림의포트폴리오</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,152 +1,375 @@
-/* 
-import React, { useEffect } from "react";
-import gsap from "gsap";
+import React from "react";
 
 function MainPage() {
-  useEffect(() => {
-    const splitText = (el) => {
-      el.innerHTML = el.textContent.replace(
-        /(\S)/g,
-        '<span class="char" style="display: inline-block; transform: translateY(100%); opacity: 0;">$1</span>'
-      );
-    };
-
-    const title = document.querySelector(".intro-title");
-
-    splitText(title);
-
-    const timeline = gsap.timeline();
-
-    // 1. 텍스트가 나타나는 애니메이션
-    timeline.to(".intro-title .char", {
-      y: 0,
-      opacity: 1,
-      stagger: 0.05,
-      ease: "bounce.out",
-      duration: 2,
-    });
-
-    // 2. `김`, `포`, `폴`을 제외한 문자 사라지기 (자리 차지 유지)
-    timeline.to(
-      ".intro-title .char",
-      {
-        opacity: (index, target) => {
-          const char = target.textContent;
-          return ["김", "포", "폴"].includes(char) ? 1 : 0;
-        },
-        duration: 1,
-        ease: "power2.out",
-      },
-      "+=1"
-    ); // 1초 대기 후 실행
-
-    // 3. 자리 차지 없애고 `김`, `포`, `폴`이 자연스럽게 붙도록 애니메이션
-    timeline.to(
-      ".intro-title",
-      {
-        letterSpacing: "0rem", // 문자 간격을 0으로 설정
-        duration: 1.5,
-        ease: "power2.out",
-      },
-      "+=0.5"
-    ); // 0.5초 대기 후 실행
-
-    // 4. `김`의 위치를 오른쪽으로 이동시키면서 자연스럽게 붙이기
-    timeline.to(
-      ".intro-title .char",
-      {
-        x: (index, target) => {
-          const char = target.textContent;
-          if (char === "김") {
-            return 55; // `김`의 x 위치를 오른쪽으로 이동
-          }
-          return 0;
-        },
-        duration: 1.5,
-        ease: "power2.out",
-        stagger: 0.1, // 각 문자들이 조금씩 붙어가는 효과
-      },
-      "+=0.5"
-    ); // 0.5초 대기 후 실행
-  }, []);
-
   return (
-    <div className="intro-container">
-      <div className="intro-content">
-        <h1 className="intro-title">김예림의포트폴리오</h1>
+    <div className="main-container">
+      <div className="bg">
+        <div className="bg-wrap">
+          <div className="bg-box bg-left"></div>
+          <div className="bg-box bg-right">
+            <div className="bg-box-inner">
+              <div className="frame">
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-}
+      <div className="fix-content fix-right">
+        <div className="f-sec">
+          <div className="fix-img def img-1 f-ani">
+            <div className="img-box">
+              <img
+                src="https://cocochi.design/wp-content/uploads/2023/02/file_21.jpg"
+                alt="img1"
+              />
+            </div>
+          </div>
+          <div className="center-txt">
+            <p className="page-heading">
+              <span className="page-sub">profile·about me </span>
+            </p>
+            <p className="name-en">김예림</p>
+          </div>
+          <a href="/" className="read_more scroll">
+            <span>r</span>
+            <span>e</span>
+            <span>a</span>
+            <span>d</span>
+            <span>&nbsp;</span>
+            <span>m</span>
+            <span>o</span>
+            <span>r</span>
+            <span>e</span>
+          </a>
+        </div>
+        <section className="content img-change" id="first-content">
+          <div className="fix-img">
+            <div className="img-box">
+              <img
+                src="https://cocochi.design/wp-content/uploads/2023/02/file_26-1-1024x768.jpg"
+                alt="img2"
+              />
+            </div>
+          </div>
+          <div className="inner-content">
+            <div className="txt-wrap">
+              <p className="sub-heading">
+                <span className="num">00</span>
+                내일
+              </p>
+              <p className="sec-heading">김예림</p>
+              <div className="sec-txt">
+                <p className="p1">주식회사 온후 대표</p>
+                <p className="p1">디자인 파트너 “여기” 주재</p>
+                <p>&nbsp;</p>
+                <p className="p1">주식회사 코스기유 커먼 디자이너</p>
+                <p>주식회사 추출사 커먼 디자이너</p>
+                <p className="p1">
+                  주식회사 목욕탕 구라시 아트 디렉터/디자이너
+                </p>
+                <p>&nbsp;</p>
+                <p className="p1">
+                  사이타마현 사이타마시 출생. 나가노현 스와시 거주{" "}
+                  <span className="s1">2022</span>년{" "}
+                  <span className="s1">11</span> 월~). 디자인계 대학을 졸업 후,
+                  디자인 제작 회사에서 그래픽 디자이너를 거쳐{" "}
+                  <span className="s1">2020</span> 년 독립. 로고 디자인을 비롯해
+                  각종 그래픽, 웹, 사인 계획 등의 「표시」를 만들고, 「여기」가
+                  즐길 때까지를 디자인할 수 있도록 시도하고 있습니다.
+                  <span className="s1">2021</span> 년부터 주식회사 고스기유와
+                  주식회사 목욕탕 구라시, 2023년부터 주식회사 추출사에 참가{" "}
+                  <span className="s1">2023</span> 년 법인화, 주식회사 온후
+                  설립.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="content img-change">
+          <div className="fix-img">
+            <div className="img-box">
+              <img
+                src="https://cocochi.design/wp-content/uploads/2023/02/file_26-1-1024x768.jpg"
+                alt="img2"
+              />
+            </div>
+          </div>
+          <div className="inner-content">
+            <div className="txt-wrap">
+              <p className="sub-heading">
+                <span className="num">00</span>
+                내일
+              </p>
+              <p className="sec-heading">김예림</p>
+              <div className="sec-txt">
+                <p className="p1">주식회사 온후 대표</p>
+                <p className="p1">디자인 파트너 “여기” 주재</p>
+                <p>&nbsp;</p>
+                <p className="p1">주식회사 코스기유 커먼 디자이너</p>
+                <p>주식회사 추출사 커먼 디자이너</p>
+                <p className="p1">
+                  주식회사 목욕탕 구라시 아트 디렉터/디자이너
+                </p>
+                <p>&nbsp;</p>
+                <p className="p1">
+                  사이타마현 사이타마시 출생. 나가노현 스와시 거주{" "}
+                  <span className="s1">2022</span>년{" "}
+                  <span className="s1">11</span> 월~). 디자인계 대학을 졸업 후,
+                  디자인 제작 회사에서 그래픽 디자이너를 거쳐{" "}
+                  <span className="s1">2020</span> 년 독립. 로고 디자인을 비롯해
+                  각종 그래픽, 웹, 사인 계획 등의 「표시」를 만들고, 「여기」가
+                  즐길 때까지를 디자인할 수 있도록 시도하고 있습니다.
+                  <span className="s1">2021</span> 년부터 주식회사 고스기유와
+                  주식회사 목욕탕 구라시, 2023년부터 주식회사 추출사에 참가{" "}
+                  <span className="s1">2023</span> 년 법인화, 주식회사 온후
+                  설립.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="content img-change">
+          <div className="fix-img">
+            <div className="img-box">
+              <img
+                src="https://cocochi.design/wp-content/uploads/2023/02/file_26-1-1024x768.jpg"
+                alt="img2"
+              />
+            </div>
+          </div>
+          <div className="inner-content">
+            <div className="txt-wrap">
+              <p className="sub-heading">
+                <span className="num">00</span>
+                내일
+              </p>
+              <p className="sec-heading">김예림</p>
+              <div className="sec-txt">
+                <p className="p1">주식회사 온후 대표</p>
+                <p className="p1">디자인 파트너 “여기” 주재</p>
+                <p>&nbsp;</p>
+                <p className="p1">주식회사 코스기유 커먼 디자이너</p>
+                <p>주식회사 추출사 커먼 디자이너</p>
+                <p className="p1">
+                  주식회사 목욕탕 구라시 아트 디렉터/디자이너
+                </p>
+                <p>&nbsp;</p>
+                <p className="p1">
+                  사이타마현 사이타마시 출생. 나가노현 스와시 거주{" "}
+                  <span className="s1">2022</span>년{" "}
+                  <span className="s1">11</span> 월~). 디자인계 대학을 졸업 후,
+                  디자인 제작 회사에서 그래픽 디자이너를 거쳐{" "}
+                  <span className="s1">2020</span> 년 독립. 로고 디자인을 비롯해
+                  각종 그래픽, 웹, 사인 계획 등의 「표시」를 만들고, 「여기」가
+                  즐길 때까지를 디자인할 수 있도록 시도하고 있습니다.
+                  <span className="s1">2021</span> 년부터 주식회사 고스기유와
+                  주식회사 목욕탕 구라시, 2023년부터 주식회사 추출사에 참가{" "}
+                  <span className="s1">2023</span> 년 법인화, 주식회사 온후
+                  설립.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </section>
 
-export default MainPage;
- */
-import React, { useEffect } from "react";
-import gsap from "gsap";
+        <section className="other-content">
+          <div className="other-box">
+            <p className="tit inline">
+              <img
+                src="https://cocochi.design/wp-content/themes/cocochi/images/txt_partners.svg"
+                alt="img3"
+              />
+            </p>
+            <div class="flex-box">
+              <p class="box02 txt">다카기 야스히라 웹 디자이너</p>
+              <p class="box02 txt">이노우 마사히로 편집자</p>
+              <p class="box02 txt">카토 오마사 스쿠</p>
+              <p class="box02 txt">카와무라 에히코 웹 엔지니어</p>
+              <p class="box02 txt">나가미네 쇼타 웹 엔지니어</p>
+              <p class="box02 txt">후지와라 케이 포토그래퍼</p>
+              <p class="box02 txt">折茂彰弘 크리에이티브 디렉터</p>
+              <p class="box02 txt">엔 피츠오 레터링 아티스트</p>
+              <p class="box02 txt">히라마츠 유스케 고스기유 3대째</p>
+              <p class="box02 txt">오야마 카즈히로 차 리스타</p>
+              <p class="box02 txt">오자와 히로유키 프로듀서</p>
+            </div>
+          </div>
+          <div className="other-box">
+            <p className="tit inline">
+              <img
+                src="https://cocochi.design/wp-content/themes/cocochi/images/txt_partners.svg"
+                alt="img3"
+              />
+            </p>
+            <div class="flex-box">
+              <p class="box02 txt">다카기 야스히라 웹 디자이너</p>
+              <p class="box02 txt">이노우 마사히로 편집자</p>
+              <p class="box02 txt">카토 오마사 스쿠</p>
+              <p class="box02 txt">카와무라 에히코 웹 엔지니어</p>
+              <p class="box02 txt">나가미네 쇼타 웹 엔지니어</p>
+              <p class="box02 txt">후지와라 케이 포토그래퍼</p>
+              <p class="box02 txt">折茂彰弘 크리에이티브 디렉터</p>
+              <p class="box02 txt">엔 피츠오 레터링 아티스트</p>
+              <p class="box02 txt">히라마츠 유스케 고스기유 3대째</p>
+              <p class="box02 txt">오야마 카즈히로 차 리스타</p>
+              <p class="box02 txt">오자와 히로유키 프로듀서</p>
+            </div>
+          </div>
+          <div className="sns-box">
+            <div className="flex-box">
+              <p className="tit">SNS</p>
+              <div className="list">
+                <ul>
+                  <li>
+                    <a href="https://twitter.com/cam_i_poipoi" class="flex-box">
+                      <div class="icon">
+                        <img
+                          src="https://cocochi.design/wp-content/uploads/2023/01/icon_twitter.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="txt-box">
+                        <p>
+                          <span>트위터</span>
+                          흐리게
+                        </p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/cam_i_poipoi/"
+                      class="flex-box"
+                    >
+                      <div class="icon">
+                        <img
+                          src="https://cocochi.design/wp-content/uploads/2023/01/icon_instagram.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="txt-box">
+                        <p>
+                          <span>Instagram @cam_i_poipoi</span>
+                          사진을 흘려
+                        </p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/poipoi_no_ie/"
+                      class="flex-box"
+                    >
+                      <div class="icon">
+                        <img
+                          src="https://cocochi.design/wp-content/uploads/2023/01/icon_instagram.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="txt-box">
+                        <p>
+                          <span>Instagram @poipoi_no_ie</span>
+                          나의 집을 흘려
+                        </p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/pencilman_pitsuwo/"
+                      class="flex-box"
+                    >
+                      <div class="icon">
+                        <img
+                          src="https://cocochi.design/wp-content/uploads/2023/01/icon_instagram.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="txt-box">
+                        <p>
+                          <span>Instagram @pencilman_pitsuwo</span>내 가장 친한
+                          친구 인 엔 피츠오입니다.
+                        </p>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
 
-function MainPage() {
-  useEffect(() => {
-    const splitText = (el) => {
-      el.innerHTML = el.textContent.replace(
-        /(\S)/g,
-        '<span class="char" style="display: inline-block; transform: translateY(100%); opacity: 0;">$1</span>'
-      );
-    };
+      <div class="bg sp_only">
+        <div class="bg-wrap">
+          <div class="bg-box-inner">
+            <div class="frame">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    const title = document.querySelector(".intro-title");
-
-    splitText(title);
-
-    const timeline = gsap.timeline();
-
-    // 1. 텍스트가 나타나는 애니메이션
-    timeline
-      .to(".intro-title .char", {
-        y: 0,
-        opacity: 1,
-        stagger: 0.05,
-        ease: "bounce.out",
-        duration: 2,
-      })
-      .to(
-        ".intro-title .char",
-        {
-          opacity: (index, target) => {
-            const char = target.textContent;
-            return ["김", "포", "폴"].includes(char) ? 1 : 0;
-          },
-          duration: 1,
-          ease: "power2.out",
-        },
-        "+=1"
-      )
-      .to(".intro-title", {
-        letterSpacing: "0rem", // 문자 간격을 0으로 설정
-        duration: 1.5,
-        ease: "power2.out",
-      })
-      .to(
-        ".intro-title .char",
-        {
-          x: (index, target) => {
-            const char = target.textContent;
-            if (char === "김") {
-              return 50; // `김`의 x 위치를 오른쪽으로 이동
-            }
-            return 0;
-          },
-          duration: 1.5,
-          ease: "power2.out",
-          stagger: 0.1, // 각 문자들이 조금씩 붙어가는 효과
-        },
-        "-=1" // `letterSpacing` 애니메이션과 동시에 시작
-      );
-  }, []);
-
-  return (
-    <div className="intro-container">
-      <div className="intro-content">
-        <h1 className="intro-title">김예림의포트폴리오</h1>
+      <div class="fix-content fix-left">
+        <div class="box-top">
+          <h1 class="header_title">
+            <a href="https://cocochi.design">
+              <img
+                src="https://cocochi.design/wp-content/themes/cocochi/images/logo_cocochi.svg"
+                alt=""
+              />
+            </a>
+          </h1>
+        </div>
+        <div class="box-middle">
+          <div class="img-box"></div>
+        </div>
+        <div class="box-bottom">
+          <nav class="nav">
+            <ul class="nav_lists">
+              <li class="nav_list">
+                <a class="nav_link" href="https://cocochi.design">
+                  집<span>홈</span>
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_link" href="https://cocochi.design/#linkThink">
+                  기모치<span>attitude</span>
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_link" href="https://cocochi.design/#linkWork">
+                  시루시<span>works</span>
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_link" href="https://cocochi.design/profile">
+                  나<span>프로필</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="nav_other">
+            <p class="text-link contact-btn">
+              <a class="fuwafuwa1" href="https://cocochi.design/contact">
+                <img
+                  src="https://cocochi.design/wp-content/themes/cocochi/images/btn_contact.svg"
+                  alt=""
+                />
+              </a>
+            </p>
+            <p class="credit">copyright onf Inc.</p>
+          </div>
+        </div>
+        <div class="nav_trigger">
+          <span></span>
+        </div>
       </div>
     </div>
   );
