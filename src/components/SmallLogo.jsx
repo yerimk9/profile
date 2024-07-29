8bee69d965f31d2a1487b11adea0911606db8439
yerimk9 <rhrh037@naver.com>
2024-07-29 12:18:56 +0900

    smallLogo 컴포넌트 스타일 분리

--- src/components/SmallLogo.jsx
+++ src/components/SmallLogo.jsx
@@ -3,25 +3,19 @@ import React from "react";
 function SmallLogo(props) {
   return (
     <div className="titleLogo-container">
-      <div className="titleLogo-content">
-        <svg
-          className="titleLogo-subTitle"
-          viewBox="0 0 450 200"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            id="curve"
-            d="M10,70 C150,20 390,10 690,65"
-            fill="transparent"
-          />
-          <text>
-            <textPath href="#curve" startOffset="50%" textAnchor="middle">
-              welcome
-            </textPath>
-          </text>
-        </svg>
-        <h1 className="titleLogo-title">김포폴</h1>
-      </div>
+      <svg
+        className="titleLogo-subTitle"
+        viewBox="0 0 450 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path id="curve" d="M10,70 C150,20 390,10 690,65" fill="transparent" />
+        <text>
+          <textPath href="#curve" startOffset="50%" textAnchor="middle">
+            welcome
+          </textPath>
+        </text>
+      </svg>
+      <h1 className="titleLogo-title">김포폴</h1>
     </div>
   );
 }
